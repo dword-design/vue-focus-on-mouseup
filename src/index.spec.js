d87492e283ef4df56ca45148dcacad1d90e09e4d
Sebastian Landwehr <info@dword-design.de>
2021-03-19 15:29:09 +0100

    fix: add puppeteer-to-istanbul for code coverage

--- src/index.spec.js
+++ src/index.spec.js
@@ -2,33 +2,33 @@ import { endent } from '@dword-design/functions'
 import puppeteer from '@dword-design/puppeteer'
 import { outputFile } from 'fs-extra'
 import { Builder, Nuxt } from 'nuxt'
+import P from 'path'
+import puppeteerToIstanbul from 'puppeteer-to-istanbul'
 import withLocalTmpDir from 'with-local-tmp-dir'
+
+const storagePath = P.resolve('.nyc_output')
 
 export default {
   valid: () =>
     withLocalTmpDir(async () => {
       await outputFile(
-        'pages/index.vue',
+        'pages/index.js',
         endent`
-          <template>
-            <button v-focus-on-mouseup>Hello world</button>
-          </template>
-          
-          <script>
           import focusOnMouseup from '../../src'
 
           export default {
             directives: { focusOnMouseup },
+            render: h => <button v-focus-on-mouseup>Hello world</button>,
           }
-          </script>
 
         `
       )
-      const nuxt = new Nuxt({ createRequire: 'native', dev: false })
+      const nuxt = new Nuxt({ createRequire: 'native', dev: true })
       await new Builder(nuxt).build()
       await nuxt.listen()
       const browser = await puppeteer.launch()
       const page = await browser.newPage()
+      await page.coverage.startJSCoverage()
       await page.goto('http://localhost:3000')
       const buttonCoords = await page.evaluate(() => {
         const button = document.querySelector('button')
@@ -48,6 +48,8 @@ export default {
       expect(await hasFocus()).toBeFalsy()
       await page.mouse.up()
       expect(await hasFocus()).toBeTruthy()
+      const coverage = await page.coverage.stopJSCoverage()
+      puppeteerToIstanbul.write(coverage, { storagePath })
       await browser.close()
       await nuxt.close()
     }),
